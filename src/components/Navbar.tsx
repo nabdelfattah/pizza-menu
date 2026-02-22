fa837fc3e2b562b548c2b42cbde28e77c0bd7891
Nada W. Abdelfattah <59118007+nabdelfattah@users.noreply.github.com>
2026-02-22 23:41:13 +0200

    feat: update header content and add styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,7 @@
-import React from "react";
-
 export default function Navbar() {
   return (
-    <header>
-      <p>nav bar here</p>
+    <header className="self-stretch">
+      <h1 className="text-[#edc84b]">Fast React Pizza Co.</h1>
     </header>
   );
 }
